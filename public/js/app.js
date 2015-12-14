338c8d5bd474dc46b8674525482f813bff8c247d
mseegan <mseegan@gmail.com>
2015-12-14 14:02:38 -0800

    added dropdown menu and form.  todo: hide/unhide form

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 	console.log("JS works!");
 
-
+$('#hiddenForm').click(function unHideCity() {
+	console.log("button is clicked");
+	document.getElementById('#cityShow').style = "display: block;";
+});
 });
