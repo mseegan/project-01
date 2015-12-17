59e044fef064d1799b94fd088319d6819151ad7e
mseegan <mseegan@gmail.com>
2015-12-17 12:02:38 -0800

    heroku stuff

--- models/index.js
+++ models/index.js
@@ -1,7 +1,14 @@
 var mongoose = require('mongoose');
-  mongoose.connect('mongodb://localhost/project-01');
+  mongoose.connect( process.env.MONGOLAB_URI ||
+                      process.env.MONGOHQ_URL || 
+                      "mongodb://localhost/project-01" );
 var city = require('./city');
 var report = require('./report'); 
 
+
+
 module.exports.City = city;
 module.exports.Report = report;
+
+
+
