dabd4aa1f09380703ec201f30c7e8250402e176e
mseegan <mseegan@gmail.com>
2015-12-17 15:30:51 -0800

    button now takes in form values and checkbox value of 'fixed'

--- public/js/appPotholes.js
+++ public/js/appPotholes.js
@@ -21,9 +21,18 @@ $(document).ready(function() {
   	$("#reportButton").click(function() {
    		event.preventDefault();
   		console.log("button pressed");
-	});
+  		var xStreet1 = $('#crossStreetForm1').val();
+  		var xStreet2 = $('#crossStreetForm2').val();
+  		var fixed = $('#fixed').val();
+  		
+  			if ($('#fixed').is(":checked")){
+  				
+  				console.log($("input[type='checkbox']").val());
+  			}
+  		
+  		
 	
-  	
+  	});
 });
 
 
