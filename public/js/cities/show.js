dd595317d85a0dbf4e92b9a05c639ff854bdf567
tgaff <tgaff@alumni.nd.edu>
2015-12-21 23:52:13 -0800

    Make generateHTML work when the user submits a new report

--- public/js/cities/show.js
+++ public/js/cities/show.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   console.log('app.js loaded!');
   var cityId = window.location.pathname.split('/')[2];
-  
+
 
 	$.ajax({
   		method: 'GET',
   		url: '/api/city/' + cityId,
   		success: function(data) {
-  			
-  			
-  			
-  			generateHtml(data);
+  			console.log('city', data);
+
+
+  			generateHtml(data.reports);
   			}
   	});
 
@@ -23,24 +23,24 @@ $(document).ready(function() {
   			data: formData,
   			success: function(data) {
   				console.log(data);
-  				$('#renderReports').html(generateHtml(data));
+  				generateHtml([data]);  // the function expects an array, but we have just one
   			}
   		});
-  		 }
+  	}
   	$("#reportButton").click(function() {
    		event.preventDefault();
   		showValues();
   	});
-  	
-  	$("#renderReports").on('click','#deleteButton',function() {
+
+  	$("#renderReports").on('click','#deleteButton', function() {
 		event.preventDefault();
 		console.log("delete button pressed");
   		var cityId = window.location.pathname.split('/')[2];
   		var id = $(this).attr("data-id");
-  		
+
 		deleteReport();
 		function deleteReport() {
-  		
+
   		$.ajax({
   			type: 'DELETE',
   			url: '/api/city/' + cityId + '/report/' + id,
@@ -58,7 +58,7 @@ $(document).ready(function() {
 	// 	console.log('button update pressed');
 	// 	var cityId = window.location.pathname.split('/')[2];
 	// 	var id = $(this).attr("data-id");
-		
+
 	// 	console.log($(this).attr("data-id"));
 	// 	function updateReport(event) {
  //  			$.ajax({
@@ -68,29 +68,31 @@ $(document).ready(function() {
 
  //  				}
  //  			});
-	
-  	
+
+
 
 
  //  	}
  //  	});
 
-});	
-function generateHtml (data){
-	console.log(data.reports);
-	
+});
+
+// this function accepts an array of reports and renders them to the page
+function generateHtml (reports){
+	console.log(reports);
+
 	// console.logs(cityId);
 
 		var source3 = $('#report-template').html();
 		// console.log(source3);
 		var template = Handlebars.compile(source3);
 		// console.log("template here:",template);
-		
-			// console.log(data);
-			var dataHtml = template({report: data.reports});
-			
-			$('#renderReports').append(dataHtml);
-		
 
-	
+		// console.log(data);
+		var dataHtml = template({reports: reports});
+
+		$('#renderReports').append(dataHtml);
+
+
+
 }
